dbfd0e607f3c5074908d9dc9ae514510327ebb6c
yurign000 <yurign000@gmail.com>
2025-09-24 01:42:33 -0300

    servidor criado

--- back/src/server.js
+++ back/src/server.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res, next) => {
+    res.header('content-type','text/html')
+    res.send("<h1>Api do Bundello</h1>")
+    
+})
+
+app.listen(3000, () => {
+    console.log('Servidor back-end iniciado');
+})
