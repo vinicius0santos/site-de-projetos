24d2af42cec7b4c84527fa28ce2a5b374df607b9
yurign000 <yurign000@gmail.com>
2025-09-25 03:15:25 -0300

    conexão com a tabela comentários criada na api

--- back/src/router/User.js
+++ back/src/router/User.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const User = require('../controller/User');
-const Model = require('../model/User')
-const hashcode = require("../lib/hashcode")
 
 router.post('/create', User.insert);
 router.post('/login', User.login);
--- back/src/server.js
+++ back/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const User = require('./router/User');
+const Comment = require('./router/Comment');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
@@ -9,6 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 app.use('/user', User);
+app.use('/comment', Comment);
 
 app.get('/', (req, res) => {
     res.header('content-type', 'text/html');
