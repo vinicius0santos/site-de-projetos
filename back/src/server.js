ea993f31c206085d29bc2e00517d0fe0aabba227
yurign000 <yurign000@gmail.com>
2025-09-24 23:22:44 -0300

    liberação de cors block

--- back/src/server.js
+++ back/src/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const User = require('./router/User');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
 app.use('/user', User);
 
 app.get('/', (req, res) => {
